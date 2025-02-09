7c8c107cfa6d10f72383e3d90fe2280748bcd37e
lmdown <lmdown@163.com>
2025-02-09 16:48:27 +0800

    feat: dir size

--- src/self-manage/router/CommonUtilRouters.ts
+++ src/self-manage/router/CommonUtilRouters.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import SystemInfoUtil from '@/util/SystemInfoUtil';
+import FileInfoUtil from '@/util/FileInfoUtil';
 
 const router = express.Router();
 
@@ -26,6 +27,15 @@ router.get('/user-home-dir', (req:Request, res:Response) => {
   const userHomeDir = process.env.HOME
   res.json({
     userHomeDir
+  });
+});
+
+router.get('/dir-file-size', (req:Request, res:Response) => {
+  const dirPath = String(req.query.dirPath)
+  const fileSize = FileInfoUtil.getDirSize(dirPath)
+  res.json({
+    dirPath,
+    fileSize
   });
 });
 
